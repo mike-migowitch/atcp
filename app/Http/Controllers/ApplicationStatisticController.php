602d3e73cc95ce33dbc5bfe40815ed122a41b552
Mikhail Migovich <migowitch@winechef.com>
2022-11-02 17:56:06 +0300

    create service and route

--- app/Http/Controllers/ApplicationStatisticController.php
+++ app/Http/Controllers/ApplicationStatisticController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\AppticaTopAppsService;
 use Illuminate\Http\Request;
 
 class ApplicationStatisticController extends Controller
 {
-    //
+    /**
+     * @var AppticaTopAppsService
+     */
+    private $topAppsService;
+
+    public function __construct(AppticaTopAppsService $topAppsService)
+    {
+        $this->topAppsService = $topAppsService;
+    }
+
+    public function getAppTopCategoryByDate(\DateTimeInterface $date)
+    {
+        $this->topAppsService->sendRequest(new \DateTime('now'));
+    }
 }
---
+++ app/Services/AppticaTopAppsService.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Services;
+
+use Http;
+
+class AppticaTopAppsService
+{
+    /**
+     * Уникальный идентификатор приложения, в тестовом задании его значение 1421444 (это приложение Among Us)
+     */
+    const APPLICATION_ID = 1421444;
+
+    /**
+     * Уникальный идентификатор страны, в тестовом задании его значение 1 (United States)
+     */
+    const COUNTRY_ID = 1;
+
+    /**
+     * Наверняка, это API токен для Apptica
+     */
+    const B4NKGg = "fVN5Q9KVOlOHDx9mOsKPAQsFBlEhBOwguLkNEDTZvKzJzT3l";
+
+    /**
+     * Парсит строку для запроса
+     * @param \DateTimeInterface $date Дата, на которую нужно получить статистику
+     * @return string
+     */
+    private function getRequestUrl(\DateTimeInterface $date): string
+    {
+        $dateString = $date->format("Y-m-d");
+
+        return sprintf("https://api.apptica.com/package/top_history/%d/%d?
+            date_from=%s&
+            date_to=%s&
+            B4NKGg=%s", self::APPLICATION_ID, self::COUNTRY_ID, $dateString, $dateString, self::B4NKGg);
+    }
+
+    public function sendRequest(\DateTimeInterface $date)
+    {
+        $url = $this->getRequestUrl($date);
+
+        $response = Http::get($url);
+    }
+}
