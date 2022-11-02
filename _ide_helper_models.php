cb7527de1277c6cc8716127b4d64b242ea45c968
Mikhail Migovich <migowitch@winechef.com>
2022-11-02 17:10:45 +0300

    added ide-helper package

---
+++ _ide_helper_models.php
@@ -0,0 +1,24 @@
+<?php
+
+// @formatter:off
+/**
+ * A helper file for your Eloquent Models
+ * Copy the phpDocs from this file to the correct Model,
+ * And remove them from this file, to prevent double declarations.
+ *
+ * @author Barry vd. Heuvel <barryvdh@gmail.com>
+ */
+
+
+namespace App\Models{
+/**
+ * App\Models\ApplicationStatistic
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|ApplicationStatistic newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|ApplicationStatistic newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|ApplicationStatistic query()
+ * @mixin \Eloquent
+ */
+	class IdeHelperApplicationStatistic {}
+}
+
--- app/Models/ApplicationStatistic.php
+++ app/Models/ApplicationStatistic.php
@@ -5,6 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @mixin IdeHelperApplicationStatistic
+ */
 class ApplicationStatistic extends Model
 {
     use HasFactory;
